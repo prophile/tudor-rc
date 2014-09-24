2db02f0d4374e5990da6eb50f5f72f465fabca8e
Alistair Lynn <arplynn@gmail.com>
2014-09-24 21:54:12 +0100

    Add a system to connect to Henry

--- run.py
+++ run.py
@@ -2,17 +2,27 @@ import time
 from collections import namedtuple
 import serial
 
-PORT_LEFT = '/dev/tty.usbserial-SR0GK1A'
-PORT_RIGHT = '/dev/tty.usbserial-SR0UK1L'
+PORT_RIGHT = '/dev/tty.usbserial-SR0GK1A'
+PORT_LEFT = '/dev/tty.usbserial-SR0UK1L'
 
 Forward = namedtuple('Forward', 'speed')
 Reverse = namedtuple('Reverse', 'speed')
 Freewheel = object()
 Brake = object()
 
+def invert(channel):
+    def new_channel(x):
+        if isinstance(x, Forward):
+            channel(Reverse(speed=x.speed))
+        elif isinstance(x, Reverse):
+            channel(Forward(speed=x.speed))
+        else:
+            channel(x)
+    return new_channel
+
 class MotorBoard:
     def __init__(self, port):
-        self._connection = serial.Serial(port, baudrate=1000000)
+        self._connection = serial.Serial(port, baudrate=115200)
         # FIXME: ugliest hack of all time
         time.sleep(2)
         self.channels = (self._channel(0), self._channel(1))
@@ -36,6 +46,23 @@ class MotorBoard:
 BOARD_LEFT = MotorBoard(PORT_LEFT)
 BOARD_RIGHT = MotorBoard(PORT_RIGHT)
 
-BOARD_LEFT[0](Forward(100))
-time.sleep(10)
+print('GOING')
+
+BACK = invert(BOARD_LEFT[0])
+FRONT_LEFT = invert(BOARD_RIGHT[1])
+FRONT_RIGHT = BOARD_RIGHT[0]
+
+FRONT_LEFT(Forward(30))
+FRONT_RIGHT(Forward(30))
+BACK(Brake)
+
+time.sleep(3)
+
+FRONT_LEFT(Brake)
+FRONT_RIGHT(Brake)
+BACK(Forward(30))
+
+time.sleep(2)
+
+BACK(Brake)
 
