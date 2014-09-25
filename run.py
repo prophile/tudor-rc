a369b63c2815612971164b13a4995c62cdc1c7a4
Alistair Lynn <arplynn@gmail.com>
2014-09-25 10:58:42 +0100

    Accept control from the joystick program

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import time
 from collections import namedtuple
 import serial
+import json
+import subprocess
+import sys
 
 PORT_RIGHT = '/dev/tty.usbserial-SR0GK1A'
 PORT_LEFT = '/dev/tty.usbserial-SR0UK1L'
@@ -34,9 +37,9 @@ class MotorBoard:
         def drive(value):
             self._connection.write((channel + 2,))
             if isinstance(value, Forward):
-                self._connection.write((128 + value.speed,))
+                self._connection.write((128 + int(value.speed),))
             if isinstance(value, Reverse):
-                self._connection.write((128 - value.speed,))
+                self._connection.write((128 - int(value.speed),))
             if value is Freewheel:
                 self._connection.write((128,))
             if value is Brake:
@@ -48,21 +51,41 @@ BOARD_RIGHT = MotorBoard(PORT_RIGHT)
 
 print('GOING')
 
+proc = subprocess.Popen('./joystick',
+                        stdout=subprocess.PIPE,
+                        stderr=sys.stderr)
+
+def set(channel, level):
+    if level < -1:
+        level = -1
+    if level > 1:
+        level = 1
+    if level < 0:
+        channel(Reverse(-level * 80))
+    elif level > 0:
+        channel(Forward(level * 80))
+    else:
+        channel(Freewheel)
+
 BACK = invert(BOARD_LEFT[0])
 FRONT_LEFT = invert(BOARD_RIGHT[1])
 FRONT_RIGHT = BOARD_RIGHT[0]
 
-FRONT_LEFT(Forward(30))
-FRONT_RIGHT(Forward(30))
-BACK(Brake)
-
-time.sleep(3)
+for line in proc.stdout:
+    (x, y, kill) = json.loads(line.decode('utf-8'))
+    if kill:
+        FRONT_LEFT(Brake)
+        FRONT_RIGHT(Brake)
+        BACK(Brake)
+    else:
+        xs = x / 32768
+        ys = y / 32768
+        print(xs, ys)
+        set(FRONT_LEFT, ys)
+        set(FRONT_RIGHT, ys)
+        set(BACK, xs)
 
 FRONT_LEFT(Brake)
 FRONT_RIGHT(Brake)
-BACK(Forward(30))
-
-time.sleep(2)
-
 BACK(Brake)
 
